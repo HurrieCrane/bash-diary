4dd44bd96a140855af568d59ca944d11a607b396
Joseph Crane <the.cranejoe@gmail.com>
2019-12-09 11:54:51 +0000

    I've added an actual help message

--- diary.sh
+++ diary.sh
@@ -15,10 +15,24 @@ ARGS=$(getopt -s bash --options $SHORTOPTS --longoptions $LONGOPTS --name $PROGN
 # Application argument values
 DIARY_NAME=""
 
+# Help function
+print_help() {
+  printf "Usage \e[92mdiary.sh command\e[0m\n"
+  printf "\n"
+  printf "Commands:\n"
+  printf "\n-h|--help:\n"
+  printf "\tThis command, it prints helpful information to the console\n"
+  printf "\n-n|--name:\n"
+  printf "\tThis command appends an additional name to the end of that diary entry,\n"
+  printf "\tfor example 00-00-00_ExtraName\n"
+  printf "\n\n"
+}
+
+
 while [[ $1 != '' ]]; do
   case $1 in
       -h|--help)
-        printf ""
+        print_help
         exit 0
       ;;
       -n|--name)
